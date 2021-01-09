1393f4dbaaa3a430d886368cb90c63dae186cb3d
jon apagueno <jonathan.apagueno@gmail.com>
2021-01-09 15:09:44 -0800

    adds basic csr

--- src/App.js
+++ src/App.js
@@ -1,12 +1,39 @@
+import { useState } from 'react';
+
+// import { getUser, logout } from './services/userService';
+
 import Footer from './components/Footer';
 import Header from './components/Header';
+
+import HomePage from './pages/HomePage';
+import DashboardPage from './pages/DashboardPage';
+import LoginPage from './pages/LoginPage';
+import SignupPage from './pages/SignupPage';
+
 import './App.css';
+
+import { Redirect, Route, Switch, withRouter } from 'react-router-dom'
 
 function App() {
   return (
     <div className="App">
       <Header />
-        <p>VGC</p>
+        <main>
+          <Switch>
+            <Route exact path="/" render={props =>
+              <HomePage />
+            } />
+            <Route exact path="/dashboard" render={props =>
+              <DashboardPage />
+            } />
+            <Route exact path="/signup" render={props =>
+              <SignupPage />
+            } />
+            <Route exact path="/login" render={props =>
+              <LoginPage />
+            } />
+          </Switch>
+        </main>
       <Footer />
     </div>
   );
