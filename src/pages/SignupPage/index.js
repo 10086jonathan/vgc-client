67134838848b793108d45d1cbb942039a5c2d379
jon apagueno <jonathan.apagueno@gmail.com>
2021-01-11 23:34:01 -0800

    adds controlled component

--- src/pages/SignupPage/index.js
+++ src/pages/SignupPage/index.js
@@ -1,7 +1,79 @@
+import { useState } from 'react';
+// import { signup } from '../../services/userService';
+
+import { Link } from 'react-router-dom';
+
 function SignupPage(props) {
+
+    const [formState, setFormState] = useState(getInitialFormState());
+
+    function getInitialFormState() {
+        return {
+            firstName: "",
+            lastName: "",
+            email: "",
+            password: "",
+        }
+    }
+
+    function handleChange(event) {
+        setFormState(prevState => ({
+            ...prevState,
+            [event.target.name]: event.target.value
+        }));
+    };
+
+    async function handleSubmit(event) {
+        try {
+            event.preventDefault();
+
+            console.log(formState);
+
+        //     await signup(formState);
+
+            setFormState(getInitialFormState());
+
+        //     props.handleSignupOrLogin();
+
+            props.history.push('/dashboard');
+
+        } catch (error) {
+            alert(error.message)
+        }
+    };
+
     return (
-        <div>
-            SignupPage
+        <div className="Page">
+            <form onSubmit={handleSubmit}>
+                <div>
+                    <input
+                        value={formState.firstName}
+                        onChange={handleChange}
+                        name="firstName"
+                        type="text"
+                    />
+                    <input
+                        value={formState.lastName}
+                        onChange={handleChange}
+                        name="lastName"
+                        type="text"
+                    />
+                    <input
+                        value={formState.email}
+                        onChange={handleChange}
+                        name="email"
+                        type="email"
+                    />
+                    <input
+                        value={formState.password}
+                        onChange={handleChange}
+                        name="password"
+                        type="password"
+                    />
+                    <button>Sign Up</button>
+                    <p><Link to="/">Cancel</Link></p>
+                </div>
+            </form>
         </div>
     );
 }
