726edcfff94c0c0ab16e6753cba358dff25f4cbc
jon apagueno <jonathan.apagueno@gmail.com>
2021-01-12 21:55:01 -0800

    adds ternary expression

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -9,18 +9,28 @@ function Header(props) {
             </Link>
              <nav>
                <ul>
-                   <li>
-                       <Link to="/">Logout</Link>
-                    </li>
-                   <li>
-                       <Link to="/Dashboard">Dashboard</Link>
-                    </li>
-                   <li>
-                       <Link to="/login">Login</Link>
-                    </li>
-                   <li>
-                       <Link to="/signup">Signup</Link>
-                    </li>
+                   {
+                       props.user ?
+                        <>
+                            <li>
+                                <Link to="/" onClick={props.handleLogout} >Logout</Link>
+                            </li>
+                            <li>
+                                <Link to="/Dashboard">Dashboard</Link>
+                            </li>
+                        </>
+
+                        :
+
+                        <>
+                            <li>
+                                <Link to="/login">Login</Link>
+                            </li>
+                            <li>
+                                <Link to="/signup">Signup</Link>
+                            </li>
+                        </>
+                   }
                </ul>
            </nav>
         </header>
