117bfc19fd17208e0676fdf30d8a98b122e8b8bf
jon apagueno <jonathan.apagueno@gmail.com>
2021-01-12 18:01:18 -0800

    adds func to loginPage

--- src/pages/LoginPage/index.js
+++ src/pages/LoginPage/index.js
@@ -1,9 +1,63 @@
+import { useState } from 'react';
+import { login } from '../../services/userService';
+
 function LoginPage(props) {
+    const [formState, setFormState] = useState(getInitialFormState());
+
+    function getInitialFormState() {
+        return {
+            firstName: "",
+            lastName: "",
+            email: "",
+            password: ""
+        }
+    }
+
+    function handleChange(event) {
+        setFormState(prevState => ({
+            ...prevState,
+            [event.target.name]: event.target.value
+        }));
+    };
+    
+    async function handleSubmit(event) {
+        try {
+            event.preventDefault();
+
+            await login(formState);
+
+            setFormState(getInitialFormState());
+
+            props.handleSignupOrLogin();
+
+            props.history.push('/dashboard');
+
+        } catch (error) {
+            alert(error.message)
+        }
+    };
+
     return (
-        <div>
-            LoginPage
+        <div className="Page">
+            <form onSubmit={handleSubmit}>
+                <div>
+                    <input
+                      value={formState.email}
+                      onChange={handleChange}
+                      name="email"
+                      type="email"
+                    />
+                    <input
+                      value={formState.password}
+                      onChange={handleChange}
+                      name="password"
+                      type="password"
+                    />
+                    <button>Login</button>
+                </div>
+            </form>
         </div>
     );
-}
+};
 
 export default LoginPage;
