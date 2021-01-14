769344a5f0ea99cdaa1d0d113baded88ffbff932
jon apagueno <jonathan.apagueno@gmail.com>
2021-01-13 19:18:51 -0800

    adds background img to card

---
+++ src/components/VideoGameCard/index.js
@@ -0,0 +1,9 @@
+function Cards(props) {
+    return(
+        <article>
+            <img src={props.game.background_image} alt="background image" />
+        </article>
+    )
+}
+
+export default Cards;
