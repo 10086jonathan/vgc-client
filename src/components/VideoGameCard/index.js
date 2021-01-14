d1c1277a2bc6a4dfaa32f268d3f02e9f52e99463
jon apagueno <jonathan.apagueno@gmail.com>
2021-01-13 22:06:12 -0800

    adds vgame card img and css module

--- src/components/VideoGameCard/index.js
+++ src/components/VideoGameCard/index.js
@@ -1,8 +1,18 @@
+import styles from './VideoGameCard.module.css';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Card from 'react-bootstrap/Card';
+
 function Cards(props) {
     return(
-        <article>
-            <img src={props.game.background_image} alt="background image" />
-        </article>
+        <Card className={styles.Card} >
+            <Card.Body>
+                <Card.Text>
+                    {props.game.name}
+                </Card.Text>
+            <Card.Img className={styles.CardImg} src={props.game.background_image} alt="Video Game Background" />
+            </Card.Body>
+        </Card>
     )
 }
 
