08c886890dc6dfbc7c5cbb21877dc0da6c4e6d63
jon apagueno <jonathan.apagueno@gmail.com>
2021-01-19 21:34:06 -0800

    adds dates parameter to fetch

--- src/services/rawg-api.js
+++ src/services/rawg-api.js
@@ -5,5 +5,5 @@ const API_KEY = 'ff9015363eee4b99a8e3062ea9df8607';
 // games?key=YOUR_API_KEY
 
 export function getVideoGames() {
-    return fetch(BASE_URL + 'games?key=' + API_KEY).then(res => res.json());
+    return fetch(BASE_URL + 'games?key=' + API_KEY + '&dates=2020-01-01,2020-12-31').then(res => res.json());
 }
