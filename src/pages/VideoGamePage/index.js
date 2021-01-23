7109f1efbc0ca4411bd5bcff57782ac0420688df
jon apagueno <jonathan.apagueno@gmail.com>
2021-01-22 20:09:42 -0800

    adds comments component to page

--- src/pages/VideoGamePage/index.js
+++ src/pages/VideoGamePage/index.js
@@ -1,10 +1,12 @@
 import VideoGameInfo from '../../components/VideoGameInfo';
+import VideoGameComments from '../../components/VideoGameComments';
 
 function VideoGamePage(props) {
     
     return (
         <div>
-            <VideoGameInfo {...props}/>
+            <VideoGameInfo {...props} />
+            <VideoGameComments {...props} />
         </div>
     )
 }
